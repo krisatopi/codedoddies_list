259ac1359b183d757db265a74e667f3f20ee96b5
Krisa Topi <kliseldatopi@gmail.com>
2020-05-01 23:19:26 +0200

    Fixes

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from . import views, admin
+from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('admin/', admin.site.urls),
-    path('new_search/', views.new_search, name='new_search'),
+    path('new_search', views.new_search, name='new_search'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,9 +1,6 @@
-from urllib.parse import quote_plus
-
 import requests
 from bs4 import BeautifulSoup
 from django.shortcuts import render
-
 from . import models
 
 BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
@@ -17,7 +14,7 @@ def home(request):
 def new_search(request):
     search = request.POST.get('search')
     models.Search.objects.create(search=search)
-    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    final_url = BASE_CRAIGSLIST_URL
     response = requests.get(final_url)
     data = response.text
     soup = BeautifulSoup(data, features='html.parser')
